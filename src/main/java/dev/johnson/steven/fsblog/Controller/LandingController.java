6c753ac3eb4039d6eb8210bf9eb6107e767a5d84
Steven Johnson <johnson.stevenwilliam@gmail.com>
2024-04-23 05:04:15 -0500

    Add mappings for get request
    Added the get mapping for the root path to the
    controller method that will get the index page. Not
    sure how this was working automatically without the annotation.
    Spring magic defaults?

--- src/main/java/dev/johnson/steven/fsblog/Controller/LandingController.java
+++ src/main/java/dev/johnson/steven/fsblog/Controller/LandingController.java
@@ -1,9 +1,12 @@
 package dev.johnson.steven.fsblog.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LandingController {
+
+    @GetMapping("/")
     public String getLandingPage() {
         return "index";
     }
